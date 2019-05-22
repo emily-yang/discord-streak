b66ccb019603e90e668e079d9774648897f94fb4
emily-yang <cigoua@gmail.com>
2019-05-22 08:02:41 -0700

    new database skeleton

--- db/Database-sql.js
+++ db/Database-sql.js
@@ -1,32 +1,48 @@
+/* eslint-disable spaced-comment */
 require('dotenv').config();
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+const dbFile = './.data/sqlite.db';
+const exists = fs.existsSync(dbFile);
 
 class Database {
   constructor(dbName) {
-    if (!dbName) this.dbName = 'discord-streak';
+    if (!dbName) this.dbName = 'discord-streak-sql';
     else this.dbName = dbName;
     this.conn = null;
     this.connected = false;
   }
 
+  /********************
+   *    Connections   *
+   ********************/
   connect() {}
 
   disconnect() {}
+
+  /****************
+   *    Players   *
+   ***************/
 
   async addPlayer(userId, userName) {
     if (!this.connected) {
       this.connect();
     }
     try {
+      // return await Player.create({ userId, userName });
+      //
     } catch (err) {
       console.error(err);
     }
   }
 
-  async findPlayer(userId) {
+  async getPlayer(userId) {
     if (!this.connected) {
       this.connect();
     }
     try {
+      // return await Player.findOne({ userId });
     } catch (err) {
       console.error(err);
     }
@@ -37,16 +53,48 @@ class Database {
       this.connect();
     }
     try {
+      // return await Player.find().sort({ maxStreak: -1 });
     } catch (err) {
       console.error(err);
     }
   }
 
-  async updatePlayerMaxStreak(userId) {
+  async incPlayerMaxStreak(userId, reportId) {
     if (!this.connected) {
       this.connect();
     }
     try {
+      // return await Player.findOneAndUpdate({ userId }, { $inc: { maxStreak: 1 }, $push: { streakMatches: reportId } });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async decPlayerMaxStreak(userId) {
+    if (!this.connected) {
+      this.connect();
+    }
+    try {
+      // eslint-disable-next-line prettier/prettier
+      // // return await Player.findOneAndUpdate(
+      //   { userId },
+      //   { $inc: { maxStreak: -1 } ,
+      //    $pop: { streakMatches: 1 } });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  /****************
+   *    Reports   *
+   ***************/
+
+  async getReports() {
+    if (!this.connected) {
+      this.connect();
+    }
+    try {
+      // return await Report.find().sort({ timestamp: -1 });
     } catch (err) {
       console.error(err);
     }
@@ -57,6 +105,7 @@ class Database {
       this.connect();
     }
     try {
+      // return await Report.findOne().sort({ timestamp: -1 });
     } catch (err) {
       console.error(err);
     }
@@ -67,24 +116,42 @@ class Database {
       this.connect();
     }
     try {
-      // get winner from most recent report
-      const lastReport = null;
-      // if same winner, increment current streak
-      // else set current streak to 1
-      const streak = lastReport && userId === lastReport.winner.userId ? lastReport.streak + 1 : 1;
-      // add report
+      // // get winner from most recent report
+      // const lastReport = await this.getLastReport();
+      // // if same winner, increment current streak
+      // // else set current streak to 1
+      // const streak = lastReport && userId === lastReport.winner.userId ? lastReport.streak + 1 : 1;
+      // // add report
+      // return await Report.create({ winner: { userId, userName }, reportedBy, streak });
     } catch (err) {
       console.error(err);
     }
   }
+
+  async deleteLastReport() {
+    if (!this.connected) {
+      this.connect();
+    }
+    try {
+      // return await Report.findOneAndDelete({}, { sort: { _id: -1 } });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  /***************
+   *    Resets   *
+   **************/
 
   async resetStandings() {
     if (!this.connected) {
       this.connect();
     }
     try {
-      // set all player streaks to 0
-      // drop reports
+      // await Player.updateMany({}, { maxStreak: 0, streakMatches: [] });
+      // await this.conn.db.dropCollection('reports', (err, result) => {
+      //   if (err) console.error(err);
+      // });
     } catch (err) {
       console.error(err);
     }
