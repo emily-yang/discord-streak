7056250d2092e83dc8132d99b980bdf38cf12c4b
emily-yang <cigoua@gmail.com>
2019-05-26 23:43:08 -0700

    remove autoincrementing id from match table

--- db/MatchRepository.js
+++ db/MatchRepository.js
@@ -6,7 +6,6 @@ class MatchRepository {
   createTable() {
     const sql = `
     CREATE TABLE IF NOT EXISTS matches (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
       streak INTEGER,
       winner TEXT references PLAYERS(id),
       datetime TEXT
@@ -14,11 +13,11 @@ class MatchRepository {
     return this.dao.run(sql);
   }
 
-  create(id, streak, winner) {
+  create(streak, winner) {
     return this.dao.run(
-      `INSERT INTO matches (id, streak, winner, datetime)
-      VALUES (?, ?, ?, DATETIME('now'))`,
-      [id, streak, winner]
+      `INSERT INTO matches (streak, winner, datetime)
+      VALUES (?, ?, DATETIME('now'))`,
+      [streak, winner]
     );
   }
 }
